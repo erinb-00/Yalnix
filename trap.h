ab5f4e15cb35f9c2014053bb91cd481fe35a9aba
erinb-00 <eungyeol.bae.th@dartmouth.edu>
2025-05-03 21:00:16 -0400

    saturday

---
+++ trap.h
@@ -0,0 +1,19 @@
+
+#ifndef _trap_h
+#define _trap_h
+#include "hardware.h"
+#include "yalnix.h"
+
+
+void TrapKernelHandler(UserContext *uctxt);
+void TrapClockHandler(UserContext *uctxt);
+void TrapIllegalHandler(UserContext *uctxt);
+void TrapMemoryHandler(UserContext *uctxt);
+void TrapMathHandler(UserContext *uctxt);
+void TrapTtyReceiveHandler(UserContext *uctxt);
+void TrapTtyTransmitHandler(UserContext *uctxt);
+void TrapDiskHandler(UserContext *uctxt);
+void InitInterruptVectorTable(void);
+
+#endif
+// trap.h
