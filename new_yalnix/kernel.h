ee4e9b23ddee1ab42817b0f97c67f945c0980cff
erinb-00 <eungyeol.bae.th@dartmouth.edu>
2025-05-07 02:59:50 -0400

    setbrk

--- new_yalnix/kernel.h
+++ new_yalnix/kernel.h
@@ -28,24 +28,46 @@
 typedef void (*trap_handler)(UserContext *);
 
 // Kernel Functions
-
+/**
+ * @brief getFreeFrame: Gets a free frame and returns its number
+ * @return int 8*i+j, where i is the byte index and j is the bit index in the bitmap
+ */
 int getFreeFrame(void);
 
+/**
+ * @brief freeFrame: Frees a frame by clearing the corresponding bit in the bitmap
+ * @param frame: The frame number to be freed
+ */
 void freeFrame(int frame);
 
+/**
+ * @brief useFrame: Marks a frame as used by setting the corresponding bit in the bitmap
+ * @param frame: The frame number to be marked as used
+ */
 void useFrame(int frame);
 
+/**
+ * @brief KernelStart: Initializes the kernel and sets up the environment for the first process
+ * @param cmd_args: Command line arguments for the first process
+ * @param pmem_size: Size of physical memory
+ * @param uctxt: User context for the first process
+ */
 void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt);
+
+/** 
+ * @brief SetKernelBrk: Sets the kernel break to the specified address
+ * @param addr: The address to set the kernel break to
+ */
 int SetKernelBrk(void *addr);
+
+/**
+ * @brief InitTrapTable: Initializes the trap table with appropriate handlers
+ */
+void InitTrapTable(void);
 void KernelInit(void);
 void BootstrapInit(UserContext *uctxt);
 void LoadInitProcess(UserContext *uctxt);
 void EnableVirtualMemory(void);
-
-int SetKernelBrk(void *addr);
-
-void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt);
-
 void DoIdle(void);
 
 
